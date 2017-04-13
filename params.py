52492c265360aa506bde4e452ff12c43952d9160
Lucile Gaultier <lucile.gaultier@oceandatalab.com>
2017-04-13 12:04:56 +0200

    mask data out of the box

--- params.py
+++ params.py
@@ -27,11 +27,11 @@ config="WW3_GS"
 # 	 (Final file name is root_name_[numberofpass].nc)
 filesgrid = os.path.join(outdatadir, '{}_grid'.format(config))
 # ------ Force the computation of the satellite grid:
-makesgrid = False
+makesgrid = True
 # ------ Give a subdomain if only part of the model is needed:
 #	 (modelbox=[lon_min, lon_max, lat_min, lat_max])
 # 	 (If modelbox is None, the whole domain of the model is considered)
-modelbox =  [280.144,290.598,32.07,40.0283] 
+modelbox =  [280.144,290.598,32.07,40.0283]
 #------- Rotation speed of the antenna (in rad/min)
 rotation_speed = 3 # * 180
 #------- List of position of 12degree beams:
