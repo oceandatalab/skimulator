e938018ffa860a6141380b2d3d9d8d6f275ea927
Lucile Gaultier <lucile.gaultier@oceandatalab.com>
2017-12-12 21:56:09 +0100

    Debug without cartopy

--- doc/images/code_image/Fig3.py
+++ doc/images/code_image/Fig3.py
@@ -21,39 +21,43 @@ indatadir = '/tmp/key/project/'
 indatadir = os.path.join(indatadir, 'skim', 'skim_output')
 config="WW3_GS_6b108az"
 config="WW3_GS_8b105az"
-modeldatapath = '/mnt/data/model/ww3_gs/ww3.201109_cur.nc'
+modeldatapath = '/tmp/key/model/ww3_gs/ww3.201109_cur.nc'
 filesgrid = os.path.join(indatadir, '{}_'.format(config))
 ipass = 59
 indatapath = '{}c01_p{:03d}.nc'.format(filesgrid, ipass)
 listfile = glob.glob(indatapath)
 listfile = sorted(listfile)
 outdatadir = '../'
-modelbox = [-90, -70., 32., 40.]
+modelbox = [-72, -70., 34., 35.5]
 scale = 8
 #modelbox = [270, 290., 32., 40.]
-
+is_cartopy = False
 # Prepare figure
 pyplot.figure(figsize=(10, 15))
 projection = cartopy.crs.Mercator()
 transform = cartopy.crs.PlateCarree()
-projection = transform
-ax1 = pyplot.subplot(121, projection=projection)
-#ax.coastlines()
-ax1.add_feature(cartopy.feature.OCEAN, zorder=1)
-ax1.add_feature(cartopy.feature.LAND, zorder=1, edgecolor='black')
-#ax.set_extent([modelbox[0], modelbox[1],  modelbox[2], modelbox[3]],
-#              projection)
-ax1.set_extent([-74., -70., 34, 37], projection)
-gl = ax1.gridlines(crs=transform, draw_labels=True, color='gray',
-             linestyle='--', alpha=0.5)
-gl.xlabels_top = False
-gl.ylabels_left = False
+if is_cartopy is True:
+    projection = cartopy.crs.Mercator()
+    transform = cartopy.crs.PlateCarree()
+    #projection = transform
+    ax1 = pyplot.subplot(121, projection=projection)
+else:
+    ax1 = pyplot.subplot(121)
+if is_cartopy is True:
+    ax1.add_feature(cartopy.feature.OCEAN, zorder=1)
+    ax1.add_feature(cartopy.feature.LAND, zorder=1, edgecolor='black')
+    ax1.set_extent(modelbox, projection)
+    gl = ax1.gridlines(crs=transform, draw_labels=True, color='gray',
+                       linestyle='--', alpha=0.5)
+    gl.xlabels_top = False
+    gl.ylabels_left = False
 ax1.set_title('(a) Model, "true" velocity')
 
 data = netCDF4.Dataset(indatapath, 'r')
 datam = netCDF4.Dataset(modeldatapath, 'r')
 iifile = data['index'][:, 0]
 lon = datam['longitude'][:]
+lon[lon > 180] = lon[lon > 180] - 360
 lat = datam['latitude'][:]
 mlon, mlat = numpy.meshgrid(lon, lat)
 mu = datam['ucur'][int(iifile[0]),:, :]
@@ -63,10 +67,17 @@ numpy.ma.masked_where(mu == datam['ucur']._FillValue, mu, copy=False)
 mu[mu.mask] = 0
 numpy.ma.masked_where(mv == datam['vcur']._FillValue, mv, copy=False)
 mv[mv.mask] = 0
-pyplot.pcolor(mlon, mlat, numpy.sqrt(mu**2 + mv**2), cmap='jet',
-              transform=transform)
-pyplot.quiver(mlon[::10, ::10], mlat[::10, ::10], mu[::10, ::10],
-              mv[::10, ::10], scale=scale, transform=transform)
+s0 = 10
+if is_cartopy is True:
+    pyplot.pcolor(mlon, mlat, numpy.sqrt(mu**2 + mv**2), cmap='jet',
+                  transform=transform)
+    pyplot.quiver(mlon[::s0, ::s0], mlat[::s0, ::s0], mu[::s0, ::s0],
+                  mv[::s0, ::s0], transform=transform)
+else:
+    pyplot.pcolor(mlon, mlat, numpy.sqrt(mu**2 + mv**2), cmap='jet')
+    pyplot.quiver(mlon[::s0, ::s0], mlat[::s0, ::s0], mu[::s0, ::s0],
+                  mv[::s0, ::s0])
+
 
 
 indatapath = '{}grid_p{:03d}.nc'.format(filesgrid, ipass)
@@ -75,13 +86,10 @@ lon = data['lon'][:]
 lon[lon > 180] = lon[lon > 180] - 360
 lat = data['lat'][:]
 ax2 = pyplot.subplot(122,  projection=projection)
-#ax.coastlines()
 ax2.add_feature(cartopy.feature.OCEAN, zorder=1)
 ax2.add_feature(cartopy.feature.LAND, zorder=1, edgecolor='black')
-#ax.set_extent([modelbox[0], modelbox[1],  modelbox[2], modelbox[3]],
-#              projection)
 print(indatapath)
-ax2.set_extent([-74., -70., 34, 37], projection)
+#ax2.set_extent(modelbox, projection)
 gl = ax2.gridlines(crs=transform, draw_labels=True, color='gray',
              linestyle='--', alpha=0.5)
 gl.xlabels_top = False
@@ -96,14 +104,15 @@ corrangle = datag['radial_angle'][:]
 datau = data['u_model'][:]
 datav = data['v_model'][:]
 uur = ur * numpy.cos(corrangle)
+uur[uur.mask] = 0
 vur = ur * numpy.sin(corrangle)
+vur[vur.mask] = 0
 for i in range(numpy.shape(lon)[1]):
     style_color = '{}+'.format(listcolor[i])
     pyplot.plot(lon[:, i], lat[:, i], style_color,
-                transform=cartopy.crs.PlateCarree())
+                transform=transform)
     pyplot.quiver(lon[:, i], lat[:, i], datau[:, i], datav[:, i],
                   color='green', scale=scale, transform=transform)
-    corrangle = angle[:, i]
     pyplot.quiver(lon[:, i], lat[:, i], uur[:, i], vur[:, i], color='red',
                   scale=scale, transform=transform)
 pyplot.savefig(os.path.join(outdatadir, 'Fig3.png'))
