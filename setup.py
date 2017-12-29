12f511dfb14f89745a58b8258a8be4b92f050676
Lucile Gaultier <lucile.gaultier@oceandatalab.com>
2017-12-29 14:56:38 +0100

    Add GPLv3 copying to all files

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
-"""Build and install the SWOT Simulator for Ocean Science package."""
+"""
+Copyright (C) 2017-2018 OceanDataLab
+This file is part of skimsimulator.
+
+skimsimulator is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+skimsimulator is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with skimsimulator.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""Build and install the SKIM Simulator for Ocean Science package."""
+
 # from distutils.core import setup
 from setuptools import setup, find_packages
 import os
--- skimsimulator/cli.py
+++ skimsimulator/cli.py
@@ -1,3 +1,20 @@
+"""
+Copyright (C) 2017-2018 OceanDataLab
+This file is part of skimsimulator.
+
+skimsimulator is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+skimsimulator is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with skimsimulator.  If not, see <http://www.gnu.org/licenses/>.
+"""
 import sys
 import skimsimulator.mod_tools as mod_tools
 import argparse
