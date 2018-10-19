a2fc26f842367e2d9bc557c1fc821e9e9e41e1fa
Lucile Gaultier <lucile.gaultier@oceandatalab.com>
2018-10-19 15:43:20 +0200

    add error free plot for l2c std computation

--- test/diags_l2c.py
+++ test/diags_l2c.py
@@ -9,45 +9,78 @@ datadir_input = p.outdatadir
 datadir_output = './'
 
 config = p.config
+list_files = glob.glob(os.path.join(datadir_input, '{}_L2C_*'.format(config)))
 nac = 63
 nal = 63
 ref = {}
 skim = {}
 
+fid = Dataset(list_files[0], 'r')
+_tmp = numpy.array(fid.variables['u_ac_true'][:])
+_tmp[_tmp < -10] = numpy.nan
+fid.close()
+nac = _tmp.shape[1]
+nal = nac
+
 std_uac = numpy.zeros(nac)
 std_ual = numpy.zeros(nal)
 ntot_ac = numpy.zeros(nac)
 ntot_al = numpy.zeros(nal)
+std_uacm = numpy.zeros(nac)
+std_ualm = numpy.zeros(nal)
+ntot_acm = numpy.zeros(nac)
+ntot_alm = numpy.zeros(nal)
 
-list_files = glob.glob(os.path.join(datadir_input, '{}_L2C_*'.format(config)))
 for filev in list_files:
     fid = Dataset(filev, 'r')
-    ref['uac'] = numpy.array(fid.variables['u_ac_model'][:])
+    print(filev)
+    ref['uac'] = numpy.array(fid.variables['u_ac_true'][:])
     ref['uac'][ref['uac'] < -10] = numpy.nan
-    ref['ual'] = numpy.array(fid.variables['u_al_model'][:])
+    ref['ual'] = numpy.array(fid.variables['u_al_true'][:])
     ref['ual'][ref['ual'] < -10] = numpy.nan
+    skim['uacm'] = numpy.array(fid.variables['u_ac_model'][:])
+    skim['uacm'][skim['uacm'] < -10] = numpy.nan
+    skim['ualm'] = numpy.array(fid.variables['u_al_model'][:])
+    skim['ualm'][skim['ualm'] < -10] = numpy.nan
     skim['uac'] = numpy.array(fid.variables['u_ac_obs'][:])
     skim['uac'][skim['uac'] < -10] = numpy.nan
     skim['ual'] = numpy.array(fid.variables['u_al_obs'][:])
     skim['ual'][skim['ual'] < -10] = numpy.nan
-
-    for i in range(ref['uac'].shape[1]):
+    nuac = ref['uac'].shape[1]
+    print(nuac, nac)
+    fid.close()
+    for i in range(nuac):
         it_ac = len(numpy.where(numpy.isnan(skim['uac'][:, i]) == False)[0])
         if it_ac >= 1:
             std_uac[i] += numpy.nanstd(skim['uac'][:, i] - ref['uac'][:, i])*it_ac
             ntot_ac[i] += it_ac
         it_al = len(numpy.where(numpy.isnan(skim['ual'][:, i]) == False)[0])
         if it_al >= 1:
-          std_ual[i] += numpy.nanstd(skim['ual'][:, i] - ref['ual'][:, i])*it_al
-          ntot_al[i] += it_al
+            std_ual[i] += numpy.nanstd(skim['ual'][:, i] - ref['ual'][:, i])*it_al
+            ntot_al[i] += it_al
+        it_acm = len(numpy.where(numpy.isnan(skim['uacm'][:, i]) == False)[0])
+        if it_acm >= 1:
+            std_uacm[i] += numpy.nanstd(skim['uacm'][:, i] - ref['uac'][:, i])*it_acm
+            ntot_acm[i] += it_acm
+        it_alm = len(numpy.where(numpy.isnan(skim['ualm'][:, i]) == False)[0])
+        if it_alm >= 1:
+            std_ualm[i] += numpy.nanstd(skim['ualm'][:, i] - ref['ual'][:, i])*it_alm
+            ntot_alm[i] += it_alm
 
 std_uac = std_uac/ntot_ac
 std_ual = std_ual/ntot_al
+std_uacm = std_uacm/ntot_acm
+std_ualm = std_ualm/ntot_alm
 
-pyplot.figure()
+f, (ax1, ax2) = pyplot.subplots(1, 2, sharey= True, figsize=(12,5 ))
 xac = numpy.arange(-(nac - 1) * p.posting/2, (nac + 1)* p.posting/2, p.posting)
-pyplot.plot(xac, std_uac, 'r', label='across track')
-pyplot.plot(xac, std_ual, 'b', label='along cross')
-pyplot.title(config)
-pyplot.legend()
+ax1.plot(xac, std_uac, 'r', label='across track')
+ax1.plot(xac, std_ual, 'b', label='along cross')
+ax1.set_title('Observation {}'.format(config))
+ax1.set_ylim([0.2, 0.64])
+ax1.legend()
+ax2.plot(xac, std_uacm, 'r', label='across track')
+ax2.plot(xac, std_ualm, 'b', label='along cross')
+ax2.set_title('Error-free {}'.format(config))
+ax2.legend()
 pyplot.savefig('std_{}.png'.format(config))
